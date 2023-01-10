7b139b3237186e78f48eb2ad6777a7776f0b374e
a-ori-a <96527832+a-ori-a@users.noreply.github.com>
2023-01-10 19:08:36 +0900

    No more green, plz

--- green.py
+++ green.py
@@ -1,4 +1,7 @@
 def has_green(img):
     data = img[230]
-    data = [ x for x,y in enumerate(data) if 60 < y[0] < 78]
+    # for i in data:
+    #     print(i)
+    # exit()
+    data = [ x for x,y in enumerate(data) if 60 < y[0] < 90 and 150 < y[1] and 20 < y[2]]
     return data
