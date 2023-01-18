5f87d8bc1a4b4f6109a24e6074a5599d2dd5b18a
a-ori-a <ma_hiro25@outlook.jp>
2023-01-18 16:32:35 +0900

    added motors.py

---
+++ motors.py
@@ -0,0 +1,18 @@
+import buildhat as bh
+from time import sleep
+
+
+class Motor:
+    def __init__(self, left, right):
+        self.tank = bh.MotorPair(left, right)
+
+    def on(self, left, right):
+        self.tank.start(left, -right)
+    
+    def off():
+        self.tank.stop()
+
+tank = Motor("C", "D")
+tank.on(100, 100)
+sleep(10)
+tank.off()
