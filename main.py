b077c2e0edfd284a0786b66f45e68e06b78d22d8
a-ori-a <96527832+a-ori-a@users.noreply.github.com>
2023-01-20 10:39:28 +0900

    define display + resolution comment

--- main.py
+++ main.py
@@ -4,16 +4,20 @@ from concurrent.futures import ProcessPoolExecutor
 import image
 import intersection
 import motors
-# from lcd import lcd
+from lcd import lcd
 
 # resolution 640x480
 # 1  ~~~  640
 # ...
 # 480
+# to
+# 1  ~~~  100
+# ...
+# 480
 
 cap = cv2.VideoCapture(0)
 tank = motors.Motor("C", "D")
-# display = lcd()
+display = lcd()
 
 if not cap.isOpened():
 	print("No camera found")
@@ -40,20 +44,3 @@ while True:
 		pass # gap or out of line
 	else:
 		print("could not see anything ...") # error
-	
-
-    # cv2.putText(frame, "turn_strength : "+str(calculation.turn_strength(top[0], bottom[0])), (10,30), cv2.FONT_HERSHEY_PLAIN, 2, (12,255,0), thickness=2)
-    # cv2.putText(frame, intersection.intersection(points), (10, 80), cv2.FONT_HERSHEY_PLAIN, 2, (12, 255, 0), thickness=2)
-
-    # frame = cv2.addWeighted(frame, 0.6, hsv, 0.4, 0)
-    # image.draw(frame, [top, bottom], points)
-    # cv2.imshow("hsv", hsv)
-    # cv2.imshow("display", frame)
-    #key = cv2.waitKey(30)
-    #if key == 113:
-    #    break
-    #elif key == 115: # press S
-    #    cv2.imwrite("./photo.jpg", frame)
-
-	# display.show(cross, 0)
-	# display.line_indicator(points[2][0])
