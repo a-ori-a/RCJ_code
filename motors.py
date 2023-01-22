ee7deeeb033c62c3f49fc8fb0081c3d9473f1b1c
a-ori-a <ma_hiro25@outlook.jp>
2023-01-22 14:56:10 +0900

    limit work

--- motors.py
+++ motors.py
@@ -6,8 +6,11 @@ class Motor:
     def __init__(self, left, right):
         self.tank = bh.MotorPair(left, right)
 
+    def power_limit(self, power):
+        return max(min(100, power),-100)
+
     def on(self, left, right):
-        self.tank.start(-left, right)
+        self.tank.start(self.power_limit(-left), self.power_limit(right))
     
     def off(self):
         self.tank.stop()
