14f0c6e020b3cc06ae16c6a5a04915846efe4e86
a-ori-a <96527832+a-ori-a@users.noreply.github.com>
2023-01-27 15:05:49 +0900

    improve display modules

--- lcd.py
+++ lcd.py
@@ -2,7 +2,9 @@ import RGB1602
 
 class LCD:
     def __init__(self):
-        self.display = RGB1602.RGB1602(16, 2)
+        try:
+            self.display = RGB1602.RGB1602(16, 2)
+        except:
 
     def cursor(self, row):
         if row == 0:
@@ -12,12 +14,16 @@ class LCD:
 
     def show(self, string:str, row=None):
         if row is None:
+            self.cursor(0) # 基本的に上側に色々表示するはずだからカーソルを上に持っていくべき
             self.display.printout(string.center(16))
         else:
             self.cursor(row)
             self.display.printout(string.center(16))
     
     def line_indicator(self, center, row=1):
+        # センターは検出下線の中心のx座標で、
+        # 0~200までの値を取る可能性があるから
+        # 200で割ってディスプレイに表示可能な16文字をかけておく
         percentage = int(center/100*16)
         string = ("_"*(percentage-1)+"ooo").ljust(16, "_")
         self.show(string, row=row)
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import cv2
 from time import sleep
-from concurrent.futures import ProcessPoolExecutor
 from green import Green
 import image
 import intersection
 import motors
 from lcd import LCD
+from buildhat import DistanceSensor, Motor
 
 # resolution 640x480
 # 1  ~~~  640
@@ -18,12 +18,18 @@ from lcd import LCD
 
 cap = cv2.VideoCapture(0)
 tank = motors.Motor("C", "D")
-# display = LCD()
+catch = Motor('A')
+ds = DistanceSensor('B')
+try:
+	display = LCD()
+except:
+	print('no lcd found')
 green = Green()
 default_speed = 10
 
 if not cap.isOpened():
 	print("No camera found")
+	lcd.show('no camra')
 	exit()
 
 while True:
@@ -36,14 +42,9 @@ while True:
 while True:
 	ret, frame = cap.read()
 	hsv = image.hsv(frame)
-	# calculate power
+	# 線の検出&曲がる強さの計算
 	power = max(50, min(image.turn_strength(hsv, 380, 460), -100))
-	# with ProcessPoolExecutor() as exe:
-	# 	exe.submit(lambda img: max(50, min(image.turn_strength(img, 380, 460), -100)), hsv)
-
-	# intersection detection
-	# points = [image.detect_line(hsv, x) for x in [200, 275, 350]]
-	# cross = intersection.intersection(points)
+	# 緑の状態確認
 	green_state = green.catch_green(hsv)
 	if (line_state := intersection.intersection(hsv)) == 'straight':
 		tank.on(50+power, 50-power)
@@ -51,7 +52,7 @@ while True:
 		if green_state == "no":
 			pass # go straight
 		elif green_state == "right":
-			pass # turn right by 90 degree
+			pass # turn right by 90 degree 
 	elif line_state == 'left':
 		if green_state == "no":
 			pass # go straight
@@ -62,9 +63,3 @@ while True:
 	else:
 		print("could not see anything ...") # error
 	display.show(line_state, 0)
-	
-	# 線の場所を表示するのはデバッグ用なら良いけど
-	# 本番環境でやると二重に線の検出をすることになるし
-	# わざわざpowerの返り値を追加するのもめんどくさいのでやめた方がいいと思います
-	# 確かに
-	# display.line_indicator(image.detect_line(hsv, 460)[0])
