9db85c159ce24cd4dc2fad342c288da9447547cd
a-ori-a <96527832+a-ori-a@users.noreply.github.com>
2023-01-27 15:34:23 +0900

    run without camera

--- lcd.py
+++ lcd.py
@@ -2,17 +2,22 @@ import RGB1602
 
 class LCD:
     def __init__(self):
+        # カメラが見つからなくてもエラーを吐かないように変更
         try:
             self.display = RGB1602.RGB1602(16, 2)
+            self.open = True
         except:
+            self.open = False
 
     def cursor(self, row):
+        if not self.open: return
         if row == 0:
             self.display.setCursor(0, 0)
         elif row == 1:
             self.display.setCursor(0, 1)
 
     def show(self, string:str, row=None):
+        if not self.open: return
         if row is None:
             self.cursor(0) # 基本的に上側に色々表示するはずだからカーソルを上に持っていくべき
             self.display.printout(string.center(16))
@@ -21,6 +26,7 @@ class LCD:
             self.display.printout(string.center(16))
     
     def line_indicator(self, center, row=1):
+        if not self.open: return
         # センターは検出下線の中心のx座標で、
         # 0~200までの値を取る可能性があるから
         # 200で割ってディスプレイに表示可能な16文字をかけておく
