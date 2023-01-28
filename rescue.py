c1485aaad60c97d8ead00e9307d4690a5bcf43c3
cy-1818 <yuki1818cy@icloud.com>
2023-01-29 05:13:53 +0900

    complete

--- rescue.py
+++ rescue.py
@@ -1,5 +1,8 @@
 import cv2
 import numpy as np
+import image
+from time import sleep
+
 
 def search_ball(hsv):
     low = np.array([0, 0, 0])
@@ -15,3 +18,43 @@ def search_ball(hsv):
             ball_points += 1
         balls += ball_x / ball_points
     return balls
+
+def find_wall():
+    return False # touch
+
+box = 1
+face = 0
+
+def explore_rescuezone(tank, ds):
+    search_box = False
+    position = 50
+    while True:
+        tank.on(10,10)
+        sleep(0.1)
+        if(find_wall()):
+            tank.turn(180)
+            if search_box:
+                if box == face:
+                    tank.move(160)
+                    tank.move(-180)
+                    tank.turn(180)
+                    break
+            face += 1
+            if face == 4:
+                position -= 25
+                face = 0
+                if position < 10:
+                    position = 10
+                    search_box = True
+            tank.move(position)
+            tank.turn(90)
+    while True:
+        tank.on(10, 10)
+        sleep(0.1)
+        if image.detect_line(460)[0] != -1:
+            tank.move(400)
+            break
+        if(find_wall()):
+            tank.turn(90)
+    exit() # stop robot
+                
