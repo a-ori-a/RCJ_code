a2cc5af06209a218e7d4ebdfda8e2eb2da94624e
cy-1818 <yuki1818cy@icloud.com>
2023-01-29 02:03:00 +0900

    add function

--- rescue.py
+++ rescue.py
@@ -5,3 +5,13 @@ def search_ball(hsv):
     low = np.array([0, 0, 0])
     high = np.array([255, 30, 150]) # 要調整
     img = cv2.inRange(hsv, low, high)
+    imgEdge, contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    balls = []
+    for n in contours:
+        ball_x = 0
+        ball_points = 0
+        for m in n:
+            ball_x += m[0][0]
+            ball_points += 1
+        balls += ball_x / ball_points
+    return balls
